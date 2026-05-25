c6893573560a348540037d226d0d1e27c39a0256
Rofiq Kholid <rofiqgege99@gmail.com>
2026-05-25 13:05:38 +0700

    feat: configure Laravel application routing and middleware settings in bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,12 +6,22 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->redirectGuestsTo(env('PORTAL_LOGIN_URL', 'https://promise.summitadyawinsa.co.id/login'));
+        $middleware->trustProxies(at: '*');
+        $middleware->encryptCookies(except: [
+            'promise_auth_session'
+        ]);
+        $middleware->alias([
+            'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
+            'check.menu' => \App\Http\Middleware\CheckMenuAccess::class,
+            'log.seen' => \App\Http\Middleware\LogLastSeen::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
